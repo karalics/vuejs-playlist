7ced5d614c7469e9bc562ed47d3a7b1edfc6b270
Karalic Slavisa <slavisa.karalic@gmail.com>
2018-05-21 00:15:36 +0200

    Tut19 nested components locally registered

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
    <h1>{{ title }}</h1>
+   <h2>NAMES</h2>
    <names></names>
+   <h2>CITIES</h2>
+   <cities></cities>
   </div>
 </template>
 
 <script>
+import Cities from './Cities.vue'
+
+
 export default {
+  components: {
+    'cities': Cities
+  },
   data () {
     return {
       title: 'My First Vue.js App'
---
+++ src/Cities.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul>
+    <li v-for="city in cities">{{ city }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      cities: ['Berlin', 'Zurich', 'Belgrade', 'Bern']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
